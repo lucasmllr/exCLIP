6137ed3325a0409ac80902ec821832026ef3d56b
lucasmllr <lucasmoeller@me.com>
2025-08-11 20:01:33 +0200

    minimal expamples for openai and openclip

---
+++ minimal_example_open_clip.py
@@ -0,0 +1,24 @@
+import open_clip
+from PIL import Image
+import torch
+
+from exclip.explanation import OpenClipExplainer
+from exclip.models.tokenization import OpenClipTokenizer
+
+
+device = 'cuda:2'
+model_name = 'ViT-B-16'
+pretraining = 'laion2b_s34b_b88k'
+model, _, prep = open_clip.create_model_and_transforms(model_name, pretraining)
+model.to(device)
+model.eval()
+explainer = OpenClipExplainer(model, device=device)
+tokenizer = OpenClipTokenizer(model_name)
+
+image = Image.open("examples/dogs.jpg")
+caption = 'A white husky and a black dog running in a snow covered forest.'
+cpt_inpt = tokenizer.tokenize(caption).to(device)
+img_inpt = prep(image).unsqueeze(0).to(device)
+
+# computing explanations for all token-patch interactions between the image and caption
+interactions = explainer.explain(cpt_inpt, img_inpt, verbose=True)
--- minimal_example_openai.py
+++ minimal_example_openai.py
@@ -1,12 +1,12 @@
 import clip
 from PIL import Image
-from exclip import Explainer
-from exclip.models.tokenization import ClipTokenizer
+from exclip.explanation import OpenAIExplainer
+from exclip.models.tokenization import OpenAITokenizer
 
 device = 'cuda:1'
 model, prep = clip.load('ViT-B/16', device=device)
-tokenizer = ClipTokenizer()
-explainer = Explainer(model, device=device)
+tokenizer = OpenAITokenizer()
+explainer = OpenAIExplainer(model, device=device)
 
 image = Image.open("examples/dogs.jpg")
 caption = 'A white husky and a black dog running in a snow covered forest.'
@@ -14,4 +14,4 @@ cpt_inpt = tokenizer.tokenize(caption).to(device)
 img_inpt = prep(image).unsqueeze(0).to(device)
 
 # computing explanations for all token-patch interactions between the image and caption
-interactions = explainer.explain(cpt_inpt, img_inpt)
+interactions = explainer.explain(cpt_inpt, img_inpt, verbose=True)
